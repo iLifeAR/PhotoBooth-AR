af621961c56c88862b3a9aeffaef7d43e2f76f87
VishwaksenReddy <vishired99@gmail.com>
2024-01-25 09:19:08 +0530

    1

--- Assets/Scripts/Non-AR/ScreenShot.cs
+++ Assets/Scripts/Non-AR/ScreenShot.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.IO;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Android;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Button))]
@@ -10,40 +11,58 @@ public class ScreenShot : MonoBehaviour
 {
     [SerializeField] RectTransform PanelUI;
     [SerializeField] Grid Grid;
-    Button BT;
+    Image BT;
 
     // Start is called before the first frame update
     void Start()
     {
-        BT = GetComponent<Button>();
+        BT = GetComponent<Image>();
     }
 
-    IEnumerator TakeScreenShot()
+    private IEnumerator TakeScreenshotAndSave()
     {
         yield return new WaitForEndOfFrame();
-        string filePath = Path.Combine(Application.temporaryCachePath, "shared img.png");
 
         BT.enabled = false;
         PanelUI.gameObject.SetActive(false);
         Grid.ToggleGridVisibility(false);
-        ScreenCapture.CaptureScreenshot(filePath, 2);
-        string GallaryPath = string.Empty;
+
+        yield return new WaitForSeconds(0.25f);
+        string Filename = System.DateTime.Now.ToString("MM-dd-yy (HH-mm-ss)") + ".png";
+
+        ScreenCapture.CaptureScreenshot(Filename, 2);
+        string filePath = Path.Combine(Application.persistentDataPath, Filename);
+        while (!File.Exists(filePath))
+        {
+            yield return null;
+
+        }
+
+
         BT.enabled = true;
         PanelUI.gameObject.SetActive(true);
         Grid.ToggleGridVisibility(true);
-        Debug.Log(filePath);
-        new NativeShare().AddFile(filePath)
-            .SetSubject("Subject goes here").SetText("Hello world!").SetUrl("https://github.com/yasirkula/UnityNativeShare")
-            .SetCallback((result, shareTarget) => Debug.Log("Share result: " + result + ", selected app: " + shareTarget))
-            .Share();
 
-        byte[] FileBytes = File.ReadAllBytes(filePath);
-        NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(FileBytes, "Photobooth", System.DateTime.Now.ToString("MM-dd-yy (HH-mm-ss)") + ".png", (success, path) => GallaryPath = path);
+        string GallaryPath=string.Empty;
+        // Save the screenshot to Gallery/Photos
+        NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(File.ReadAllBytes(filePath), "Photobooth", System.DateTime.Now.ToString("MM-dd-yy (HH-mm-ss)") + ".png", (success, path) => GallaryPath=path);
+
         if (string.IsNullOrEmpty(GallaryPath))
         {
             Debug.LogError("Not Saved");
         }
+        else
+        {
+            Debug.Log("Saved at Gallary");
 
+        }
+
+
+        new NativeShare().AddFile(GallaryPath)
+    .SetCallback((result, shareTarget) => Debug.Log("Share result: " + result + ", selected app: " + shareTarget))
+    .Share();
+
+        // To avoid memory leaks
     }
 
 
@@ -55,7 +74,7 @@ public class ScreenShot : MonoBehaviour
 
     public void Capture()
     {
-        StartCoroutine(TakeScreenShot());
+        StartCoroutine(TakeScreenshotAndSave());
     }
 
     // Update is called once per frame
